04e50d540673575c0d3c69ab02f07a9fd4409b34
Victor <79508283+Vickordon@users.noreply.github.com>
2026-04-28 17:00:42 +0300

    Fix mix.exs - pin postgrex to compatible version 0.17.3

--- mix.exs
+++ mix.exs
@@ -28,7 +28,7 @@ defmodule Photographer.MixProject do
       {:phoenix, "~> 1.7.10"},
       {:phoenix_ecto, "~> 4.4"},
       {:ecto_sql, "~> 3.10"},
-      {:postgrex, ">= 0.0.0"},
+      {:postgrex, "~> 0.17.3"},
       {:phoenix_html, "~> 3.3"},
       {:phoenix_live_reload, "~> 1.4", only: :dev, runtime: false},
       {:phoenix_live_view, "~> 0.20.1"},
