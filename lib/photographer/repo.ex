7a275e62466c56430d5a3816b238ddab1afad5a3
Victor <79508283+Vickordon@users.noreply.github.com>
2026-04-28 15:55:30 +0300

    Add Repo module

--- lib/photographer/repo.ex
+++ lib/photographer/repo.ex
@@ -1,3 +1,5 @@
 defmodule Photographer.Repo do
-  use Ecto.Repo, otp_app: :photographer, adapter: Ecto.Adapters.Postgres
+  use Ecto.Repo,
+    otp_app: :photographer,
+    adapter: Ecto.Adapters.Postgres
 end
