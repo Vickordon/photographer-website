c68885fad29be6cdf7d56a5724247e815a7c650e
Victor <79508283+Vickordon@users.noreply.github.com>
2026-04-28 14:49:15 +0300

    Add Application module

---
+++ lib/photographer/application.ex
@@ -0,0 +1,17 @@
+defmodule Photographer.Application do
+  use Application
+
+  @impl true
+  def start(_type, _args) do
+    children = [
+      PhotographerWeb.Telemetry,
+      Photographer.Repo,
+      {DNSCluster, query: Application.get_env(:photographer, :dns_cluster_query) || :ignore},
+      {Phoenix.PubSub, name: Photographer.PubSub},
+      PhotographerWeb.Endpoint
+    ]
+
+    opts = [strategy: :one_for_one, name: Photographer.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+end
